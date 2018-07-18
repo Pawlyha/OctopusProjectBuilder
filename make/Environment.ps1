b3e1510262be924ddad23c660dead4bd1ca0fd74
pavlo.oliynik <woonderluck@gmail.com>
2018-07-19 00:15:30 +0300

    Updated makefile to support .net core 2 projects.

--- make/Environment.ps1
+++ make/Environment.ps1
@@ -1,1 +1,3 @@
 $VERSION = if ($Context.Version) { $Context.Version } else { '1.0.0.0' }
+$PUBLISH_DIR = "publish"
+$TARGET_PLATFORM = "win-x64"
--- make/Makefile.ps1
+++ make/Makefile.ps1
@@ -1,33 +1,48 @@
-Define-Step -Name 'Update Assembly Info' -Target 'build' -Body {
-	. (require 'psmake.mod.update-version-info')
-	Update-VersionInFile AssemblyVersion.cs $VERSION 'Version("%")'
-}
-
 Define-Step -Name 'Build' -Target 'build' -Body {
-	call $Context.NugetExe restore OctopusProjectBuilder.sln
-	call "${env:ProgramFiles(x86)}\MSBuild\14.0\Bin\msbuild.exe" OctopusProjectBuilder.sln /t:"Clean,Build" /p:Configuration=Release /m /verbosity:m /nologo /p:TreatWarningsAsErrors=true
+	$sln = "OctopusProjectBuilder.sln"
+
+	Write-Status "Cleaning solution"
+	call dotnet clean $sln --configuration "Release"
+
+	Write-Status "Building solution"
+	call dotnet build $sln --configuration "Release" --force
 }
 
 Define-Step -Name 'Tests' -Target 'build' -Body {
 	. (require 'psmake.mod.testing')
 
+	$projects = @("OctopusProjectBuilder.Uploader.Tests", "OctopusProjectBuilder.YamlReader.Tests")
 	$tests = @()
-	$tests += Define-NUnit3Tests -GroupName 'Unit Tests' -TestAssembly "*.Tests\bin\Release\*.Tests.dll"
+	$tests += Define-DotnetTests -TestProject $projects
 
 	$tests `
-        | Run-Tests -EraseReportDirectory -Cover -CodeFilter '+[OctopusProjectBuilder*]* -[*.Tests*]*' -TestFilter '*.Tests.dll' `
-        | Generate-CoverageSummary `
+        | Run-Tests -EraseReportDirectory -Cover -CodeFilter '+[OctopusProjectBuilder*]* -[*.Tests*]*'`
+        | Generate-CoverageSummary -ReportGeneratorVersion '3.1.2'`
         | Check-AcceptableCoverage -AcceptableCoverage 89
+
+}
+
+Define-Step -Name 'Publish' -Target 'build' -Body {
+	if (Test-Path -Path "$PUBLISH_DIR"){
+		Remove-Item "$PUBLISH_DIR" -Force -Recurse
+	}
+
+	$projects = @("OctopusProjectBuilder.Console", "OctopusProjectBuilder.DocGen")
+
+	foreach ($project in $projects){
+		Write-Status "Publishing $project"
+		dotnet publish "$project" -c "Release" -r "$TARGET_PLATFORM" -o "..\$PUBLISH_DIR\$project" --self-contained
+	}
 }
 
 Define-Step -Name 'Documentation generation' -Target 'build' -Body {
-	& OctopusProjectBuilder.DocGen\bin\Release\OctopusProjectBuilder.DocGen.exe | out-file Manual.md -Encoding utf8
+	call dotnet "$PUBLISH_DIR\OctopusProjectBuilder.DocGen\OctopusProjectBuilder.DocGen.dll" | out-file Manual.md -Encoding utf8
 	if ($LastExitCode -ne 0) { throw "A program execution was not successful (Exit code: $LASTEXITCODE)." }
 }
 
 Define-Step -Name 'Packaging' -Target 'build' -Body {
 	. (require 'psmake.mod.packaging')
-	
+
 	Package-DeployableNuSpec -Package 'OctopusProjectBuilder.Console.nuspec' -version $VERSION
 }
 
